b07457d7e9f716d4fea787d5d61fd882c0e08a45
vmaji <vmaji@DESKTOP-PEGQV09>
2021-10-06 01:35:40 +0100

    added new Stock addition to book

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -13,15 +13,67 @@ function AddStock(button) {
 	buildtable(button, 'AS');
 
 }
+function deleteRow(button) {
+	button.parentNode.parentNode.parentNode.deleteRow(button.parentNode.parentNode.rowIndex);
+}
+function saveRow(button) {
+	var text=button.parentNode.parentNode.innerText;
+	button.parentNode.parentNode.parentNode.deleteRow(button.parentNode.parentNode.rowIndex);
+	var table = document.getElementById("myTable");
+	var oldRow = document.getElementById("searchOutput");
+	var tt = document.createTextNode("tbody");
+	var newRow = document.createElement("tr");
+	var isbn=button.parentNode.parentNode.children[0].getElementsByTagName("input")[0].value;
+	var title=button.parentNode.parentNode.children[1].getElementsByTagName("input")[0].value
+	var auther=button.parentNode.parentNode.children[2].getElementsByTagName("input")[0].value
+	var publicationDate=button.parentNode.parentNode.children[3].getElementsByTagName("input")[0].value
+	var copies=button.parentNode.parentNode.children[4].getElementsByTagName("input")[0].value
+	var available=copies;
+	
+	newRow.innerHTML ="<tr class='table'>" +
+	
+	"<td>"+isbn+"</div></td>" +
+	"<td> "+title+"</td>" +
+	"<td>"+auther+"</td>" +
+	"<td>"+publicationDate+"</td>" +
+	"<td> "+copies+"</td>" +
+	"<td> "+available+ "</td>" +
+		"<td><div><input type='number' min='1' max='100' value='1' id='numberlist'><button id = 'movieAdded' type='button' style='display:inline-block' onclick=AddStock(this)>  <img src='img/add.png' width='25' height='25' style='float:left;margin-right:0.5em'>Add Stock</button>"+
+	 "<button id = 'movieAdded' type='button' style='display:inline-block' onclick=borrow(this)>  <img src='img/borrow.jpg' width='25' height='25' style='float:left;margin-right:0.5em'>Borrow</button>"+
+	 "<button id = 'movieAdded' type='button' style='display:inline-block' onclick=returnBook(this)>  <img src='img/return.png' width='25' height='25' style='float:left;margin-right:0.5em'>Return</button></div></td>"+
+	"</tr>";
+	oldRow.append(newRow);
+	
+}
+function addNewBook(button) {
+	event.preventDefault();
+	var table = document.getElementById("myTable");
+	var oldRow = document.getElementById("searchOutput");
+	var newRow = document.createElement("tr");
+	newRow.innerHTML = "<tr id='newRow'>" +
+			"<td> <div style='margin-right: 3px;'><input type='text' style='width:100%;' required></div></td>" +
+			"<td> <div style='margin-right: 3px;'><input type='text' style='width:100%;'></div></td>" +
+			"<td> <div style='margin-right: 3px;'><input type='text' style='width:100%;'></div></td>" +
+			"<td> <div style='margin-right: 3px;'><input type='text' style='width:100%;'></div></td>" +
+			"<td> <div style='margin-right: 3px;'><input type='number' min='1' max='100' value='1' ></div></td>" +
+			"<td> <div style='margin-right: 3px;'>&nbsp;</div></td>" +
+			"<td><button id = 'movieAdded' type='button' style='display:inline-block' onclick=saveRow(this)> <img src='img/save.png' width='25' height='25' style='float:left;margin-right:0.5em'></mg>Save</button>" +
+			"<button id = 'movieAdded' type='button' style='display:inline-block' onclick=deleteRow(this)> <img src='img/delete.png' width='25' height='25' style='float:left;margin-right:0.5em'></mg>Delete</button> </td>" +
+
+			"</tr>";
+	  table.insertBefore(newRow, oldRow);
+	
+}
 function buildtable(button, type) {
-	var isdn = button.parentNode.parentNode.firstElementChild.innerText;
-	var tabletext = button.parentNode.parentNode.parentNode.innerText
+	var isdn = button.parentNode.parentNode.parentNode.firstElementChild.innerText;
+	var tabletext = button.parentNode.parentNode.parentNode.parentNode.innerText
 			.replaceAll("Borrow\n", "").replaceAll("\t\nAdd Stock\n", "")
-			.replaceAll("Return", "").split("\n")
+			.replaceAll("Return\n"  , "").replaceAll("Return" ,"").split("\n")
 	var flag = false;
 	var data = "";
 	var index = 0;
-	for (j = 1; j < tabletext.length; j++) {
+	var numberval=button.parentNode.parentNode.firstElementChild.firstElementChild.value;
+	for (j = 0; j < tabletext.length; j++) {
 		var celltext = tabletext[j].split("\t");
 
 		for (i = 0; i < celltext.length; i++) {
@@ -52,14 +104,15 @@ function buildtable(button, type) {
 					}
 				}
 				if (type == 'AS') {
-					var available = parseInt(celltext[i]) + 1;
+					
+					var available = parseInt(celltext[i]) +  parseInt(numberval);
 					
 				}
 				celltext[i] = available;
 				data += celltext[i];
 			} else {
 				if (flag == true && type == 'AS' && i==4) {
-					celltext[i] = parseInt(celltext[i]) + 1;	
+					celltext[i] = parseInt(celltext[i]) +  parseInt(numberval);	
 				}
 
 				data += celltext[i];
@@ -83,14 +136,15 @@ function buildtable(button, type) {
 function processData(data1) {
 	var data = data1.split(/\r\n|\n/);
 
-	var table = "<table class='table'>";
+	var table = "<table class='table' id='myTable'> <thead>";
 	table += "<th> ISBN</th>";
 	table += "<th>Title</th>";
 	table += "<th>Author</th>";
 	table += "<th>Publication Date.</th>";
 	table += "<th>Copies</th>";
 	table += "<th>Available</th>";
-	table += "<th>Action</th>";
+	table += "<th>Action</th> </thead>";
+table +="<tbody id='searchOutput'>";
 	for (i = 0; i < data.length; i++) {
 		table += "<tr>";
 		var row = data[i];
@@ -99,16 +153,17 @@ function processData(data1) {
 		for (j = 0; j < cells.length; j++) {
 			table += "<td>";
 			table += cells[j];
-			table += "</th>";
+			table += "</td>";
 		}
 		table += "<td>";
-		var buttongrp = "<button id = 'movieAdded' type='button' style='display:inline-block' onclick=AddStock(this)>  <img src='img/add.png' width='25' height='25' style='float:left;margin-right:0.5em'>Add Stock</button>"
-		buttongrp += "<button id = 'movieAdded' type='button' style='display:inline-block' onclick=borrow(this)>  <img src='img/borrow.jpg' width='25' height='25' style='float:left;margin-right:0.5em'>Borrow</button>"
-		buttongrp += "<button id = 'movieAdded' type='button' style='display:inline-block' onclick=returnBook(this)>  <img src='img/return.png' width='25' height='25' style='float:left;margin-right:0.5em'>Return</button>"
+		var buttongrp =	"<div><input type='number' min='1' max='100' value='1' id='numberlist'><button id = 'movieAdded' type='button' style='display:inline-block' onclick=AddStock(this)>  <img src='img/add.png' width='25' height='25' style='float:left;margin-right:0.5em'>Add Stock</button>";
+		buttongrp += "<button id = 'movieAdded' type='button' style='display:inline-block' onclick=borrow(this)>  <img src='img/borrow.jpg' width='25' height='25' style='float:left;margin-right:0.5em'>Borrow</button>";
+		buttongrp += "<button id = 'movieAdded' type='button' style='display:inline-block' onclick=returnBook(this)>  <img src='img/return.png' width='25' height='25' style='float:left;margin-right:0.5em'>Return</button></div>";
 
 		table += buttongrp;
-		table += "</th>";
+		table += "</td>";
 	}
+	table += "</tbody>";
 	table += "</table>";
 	$("#file-parsed").html(table);
 }
